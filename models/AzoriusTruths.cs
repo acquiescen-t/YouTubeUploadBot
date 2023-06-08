23ca9ce3c7227ca900f9f285b36f80a5feb959c4
Trevor <trevorchew.kh@gmail.com>
2023-06-08 19:43:17 +0800

    Added New Deck

---
+++ models/AzoriusTruths.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace YouTubeUploadBot.models
+{
+    public class AzoriusTruths : Deck
+    {
+        public AzoriusTruths()
+        {
+            deckName = "Azorius Truths";
+            deckList = "https://mtga.untapped.gg/profile/32f5219c-4fd3-4a14-aba9-86c23e310d77/968639F8FA7FDABA/deck/99343a46-c0fb-4e1b-9b2f-d3e70ba7969e?gameType=constructed&constructedType=ranked";
+            deckTech = "Coming soon!";
+            uploadedFolder = "G:\\YouTube\\MTG\\footage\\Azorius Truths\\Uploaded";
+            thumbnailFolder = "G:\\YouTube\\MTG\\footage\\Azorius Truths\\Azorius Truths Thumbnail";
+        }
+    }
+}
